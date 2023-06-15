26954395d523c626294e7e1a74915cbe9349eb4d
Dechin Guo <mwlkgdq@outlook.com>
2023-06-15 16:22:16 +0800

    ”更新了1.2beta2，并入登录界面“

--- MMCL Launcher 1.2beta.py
+++ MMCL Launcher 1.2beta.py
@@ -8,9 +8,7 @@ from tkinter import filedialog
 from tkinter import Toplevel
 from tkinter import font
 from tkinter import ttk
-
-
-
+from tkinter import Button
 
 #下载模块链接
 
@@ -217,4 +215,118 @@ def clear_cache():
 clear_button = tk.Button(root, text="清除缓存", bg="#FF5722", fg="white", font=button_font, command=clear_cache)
 clear_button.place(x=500, y=150)
 
-root.mainloop()
+
+class LoginWindow:
+    def __init__(self, master):
+        self.master = master
+        master.title("Mojang账户登录")
+
+        self.label_username = tk.Label(master, text="用户名：")
+        self.label_username.pack()
+
+        self.entry_username = tk.Entry(master)
+        self.entry_username.pack()
+
+        self.label_password = tk.Label(master, text="密码：")
+        self.label_password.pack()
+
+        self.entry_password = tk.Entry(master, show="*")
+        self.entry_password.pack()
+
+        self.button_login = tk.Button(master, text="在线登录", command=self.online_login)
+        self.button_login.pack()
+
+        self.button_offline_login = tk.Button(master, text="离线登录", command=self.offline_login)
+        self.button_offline_login.pack()
+
+        self.button_external_login = tk.Button(master, text="外置登录", command=self.external_login)
+        self.button_external_login.pack()
+
+    def online_login(self):
+        # 获取用户名和密码
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        # 构造POST请求的数据
+        payload = {
+            "agent": {
+                "name": "Minecraft",
+                "version": 1
+            },
+            "username": username,
+            "password": password,
+            "requestUser": "true"
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        # 发送POST请求，获取响应
+        response = requests.post("https://authserver.mojang.com/authenticate", data=json.dumps(payload), headers=headers)
+
+        # 解析响应的JSON数据
+        response_json = response.json()
+
+        # 判断是否登录成功
+        if "accessToken" in response_json:
+            # 输出响应结果中的access_token
+            access_token = response_json["accessToken"]
+            print("登录成功，access_token为：", access_token)
+
+            # 将access_token写入离线登录文件
+            offline_file_path = os.path.join(os.getcwd(), "offline_token.txt")
+            with open(offline_file_path, "w") as f:
+                f.write(access_token)
+        else:
+            # 输出错误信息
+            error_message = response_json["errorMessage"]
+            print("登录失败，错误信息：", error_message)
+
+    def offline_login(self):
+        # 获取用户名
+        username = self.entry_username.get()
+
+        # 构造离线登录文件路径
+        offline_file_path = os.path.join(os.getcwd(), "offline_token.txt")
+
+        # 判断离线登录文件是否存在
+        if os.path.exists(offline_file_path):
+            # 读取离线登录文件中的access_token
+            with open(offline_file_path, "r") as f:
+                access_token = f.read()
+
+            # 输出access_token
+            print("离线登录成功，access_token为：", access_token)
+        else:
+            # 输出错误信息
+            print("离线登录失败，离线登录文件不存在或已被删除！")
+
+    def external_login(self):
+        # 获取用户名和access_token
+        username = self.entry_username.get()
+        access_token = self.entry_password.get()
+
+        # 判断access_token是否为空
+        if access_token:
+            # 构造离线登录文件路径
+            offline_file_path = os.path.join(os.getcwd(), "offline_token.txt")
+
+            # 将access_token写入离线登录文件
+            with open(offline_file_path, "w") as f:
+                f.write(access_token)
+
+            # 输出access_token
+            print("外置登录成功，access_token为：", access_token)
+        else:
+            # 输出错误信息
+            print("外置登录失败，access_token不能为空！")
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    login_window = LoginWindow(root) 
+
+    login_button = tk.Button(root, text="登录", bg="#4CAF50", fg="white", command=login_window.online_login)
+    login_button.pack()
+
+    root.mainloop()
+
--- MMCL Launcher1.1beta.py
+++ MMCL Launcher1.1beta.py
@@ -6,11 +6,23 @@ import traceback
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import Toplevel
-from tkinter import font
-from tkinter import ttk
 
 
+root = tk.Tk()
+root.title("Minecraft启动器")
 
+# 设置窗口大小和位置
+window_width = 600
+window_height = 400
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = int((screen_width - window_width) / 2)
+y = int((screen_height - window_height) / 2)
+root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+# 设置背景颜色和字体
+bg_color = "#f5f5f5"
+font = ("微软雅黑", 12)
 
 #下载模块链接
 
@@ -56,30 +68,16 @@ def get_versions():
     return versions_json
 
 
-root = tk.Tk()
-root.title("MMCL启动器")
-root.geometry("600x400")
-root.configure(bg="#212121")
-
-# 创建标签样式
-label_font = font.Font(family="微软雅黑", size=12, weight="bold")
-entry_font = font.Font(family="微软雅黑", size=10)
-button_font = font.Font(family="微软雅黑", size=12, weight="bold")
-
-# 创建标题标签
-title_label = tk.Label(root, text="MMCL启动器", fg="white", bg="#212121", font=font.Font(family="微软雅黑", size=18, weight="bold"))
-title_label.place(x=250, y=20)
-
 # 创建输入框和标签
-path_label = tk.Label(root, text="Minecraft路径：", fg="white", bg="#212121", font=label_font)
-path_label.place(x=50, y=100)
-path_entry = tk.Entry(root, width=50, font=entry_font)
-path_entry.place(x=200, y=100)
+path_label = tk.Label(root, text="Minecraft路径：", bg=bg_color, font=font)
+path_label.place(x=50, y=50)
+path_entry = tk.Entry(root, width=50, font=font)
+path_entry.place(x=200, y=50)
 
-args_label = tk.Label(root, text="启动参数：", fg="white", bg="#212121", font=label_font)
-args_label.place(x=50, y=150)
-args_entry = tk.Entry(root, width=50, font=entry_font)
-args_entry.place(x=200, y=150)
+args_label = tk.Label(root, text="启动参数：", bg=bg_color, font=font)
+args_label.place(x=50, y=100)
+args_entry = tk.Entry(root, width=50, font=font)
+args_entry.place(x=200, y=100)
 
 # 自动识别Minecraft路径
 minecraft_path = os.getenv('APPDATA') + "\\.minecraft"
@@ -95,8 +93,9 @@ if versions_json:
     # 创建版本选择框和变量
     version_var = tk.StringVar()
     version_var.set(versions_json[0]["id"])
-    versions_menu = ttk.Combobox(root, textvariable=version_var, values=list(version["id"] for version in versions_json), font=entry_font, state="readonly", width=10)
-    versions_menu.place(x=50, y=200)
+    versions_menu = tk.OptionMenu(root, version_var, *list(version["id"] for version in versions_json))
+    versions_menu.config(bg=bg_color, font=font, width=10)
+    versions_menu.place(x=50, y=150)
 
 def start_minecraft():
     path = path_entry.get().strip()
@@ -115,10 +114,9 @@ def start_minecraft():
 
     if not os.path.exists(version_path):
         # 如果版本没有下载，则弹出下载窗口并等待下载完成
-        download_window = tk.Toplevel(root)
+        download_window = Toplevel(root)
         download_window.title("下载中...")
         download_window.geometry("300x100")
-        download_window.configure(bg="#212121")
         download_window.deiconify()
 
         download_thread = threading.Thread(target=download_version, args=(download_url, version_path, download_window))
@@ -130,8 +128,7 @@ def start_minecraft():
     wait_window = tk.Toplevel(root)
     wait_window.title("启动中")
     wait_window.geometry("200x100")
-    wait_window.configure(bg="#212121")
-    wait_label = tk.Label(wait_window, text="正在启动，请稍候...", fg="white", bg="#212121", font=label_font)
+    wait_label = tk.Label(wait_window, text="正在启动，请稍候...", font=font)
     wait_label.pack(pady=20)
     wait_window.update()
 
@@ -139,18 +136,18 @@ def start_minecraft():
 
     wait_window.destroy()
 
-start_button = tk.Button(root, text="启动", bg="#4CAF50", fg="white", font=button_font, command=start_minecraft)
-start_button.place(x=50, y=250)
+start_button = tk.Button(root, text="启动", bg="#4CAF50", fg="white", font=font, command=start_minecraft)
+start_button.place(x=50, y=200)
+
 
 # 创建下载中心界面
 download_window = tk.Toplevel(root)
 download_window.title("下载中心")
 download_window.geometry("400x300")
-download_window.configure(bg="#212121")
 
 if versions_json:
     # 创建版本列表
-    versions_listbox = tk.Listbox(download_window, width=40, font=entry_font)
+    versions_listbox = tk.Listbox(download_window, width=40, font=font)
     for version in versions_json:
         versions_listbox.insert(tk.END, version["id"])
     versions_listbox.pack(side=tk.LEFT, fill=tk.BOTH)
@@ -181,7 +178,7 @@ def download_version():
     download_button.config(state=tk.NORMAL)
 
 # 创建下载按钮
-download_button = tk.Button(download_window, text="下载", bg="#4CAF50", fg="white", font=button_font, command=download_version)
+download_button = tk.Button(download_window, text="下载", bg="#4CAF50", fg="white", font=font, command=download_version)
 download_button.pack(side=tk.RIGHT)
 download_button.config(state=tk.DISABLED)
 
@@ -193,8 +190,8 @@ download_window.bind("<Map>", enable_download_button)
 
 # 创建启动器界面的下载中心按钮
 if versions_json:
-    download_button = tk.Button(root, text="下载中心", bg="#2196F3", fg="white", font=button_font, command=download_window.deiconify)
-    download_button.place(x=150, y=250)
+    download_button = tk.Button(root, text="下载中心", bg="#2196F3", fg="white", font=font, command=download_window.deiconify)
+    download_button.place(x=150, y=200)
 
 # 创建浏览按钮
 def browse():
@@ -203,8 +200,8 @@ def browse():
         path_entry.delete(0, tk.END)
         path_entry.insert(0, path)
 
-browse_button = tk.Button(root, text="浏览", bg="#2196F3", fg="white", font=button_font, command=browse)
-browse_button.place(x=500, y=100)
+browse_button = tk.Button(root, text="浏览", bg="#2196F3", fg="white", font=font, command=browse)
+browse_button.place(x=450, y=50)
 
 # 创建清除缓存按钮
 def clear_cache():
@@ -214,7 +211,7 @@ def clear_cache():
             os.remove(version_path)
     messagebox.showinfo("提示", "缓存已清除")
 
-clear_button = tk.Button(root, text="清除缓存", bg="#FF5722", fg="white", font=button_font, command=clear_cache)
-clear_button.place(x=500, y=150)
+clear_button = tk.Button(root, text="清除缓存", bg="#FF5722", fg="white", font=font, command=clear_cache)
+clear_button.place(x=450, y=100)
 
 root.mainloop()
